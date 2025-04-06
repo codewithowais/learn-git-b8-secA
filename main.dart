2e22d7c66768c40b3e5409a90dfcde8a46838897
codewithowais <codewithowais@gmail.com>
2025-04-06 13:45:44 +0500

    basic if else else if understand

--- main.dart
+++ main.dart
@@ -1,1 +1,11 @@
-void main() {}
+void main() {
+  int num1 = 10;
+  int num2 = 10;
+  if (num1 != num2) {
+    print("if block working...");
+  } else if (num1 == num2) {
+    print("else if block working...");
+  } else {
+    print("else block working...");
+  }
+}
