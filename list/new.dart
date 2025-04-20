d208f93a6706a1645e7c4f744bcbca912dee5c87
codewithowais <codewithowais@gmail.com>
2025-04-20 14:41:08 +0500

    SEC A

--- list/new.dart
+++ list/new.dart
@@ -8,5 +8,41 @@ void main() {
     "Ali",
     "Abdullah",
   ];
-  print(students.length);
+  print(students);
+  // students[3] = "ahmed";
+  // students.replaceRange(1, 3, ["Hadi", "Kamal", "Shareef", "AShraf"]);
+  // students.replaceRange(1, 3, ["Hadi"]);
+  // print(students);
+  // students.sort();
+  // print(students);
+  // print(students.isEmpty);
+  // print(students.isNotEmpty);
+
+  // students.add("Hadi");
+  // students.insert(3, "Hadi Kamal");
+  students.addAll(["Hadi"]);
+  students.insertAll(3, ["Hadi Kamal"]);
+  print(students);
+  var reversedStudents = List.of(students.reversed);
+  print(reversedStudents);
+  // students.remove("Ahmed");
+  // students.removeAt(3);
+  // students.removeLast();
+
+  students.clear();
+
+  Map studnet1 = {
+    "rollNo": "RN0001",
+    "name": "Ahmed",
+    "fName": "Ali",
+    "Grade": "7",
+  };
+  print(studnet1);
+  print(studnet1['rollNo']);
+  print(studnet1.keys);
+  print(studnet1.values);
+  print(studnet1.isEmpty);
+  print(studnet1.isNotEmpty);
+
+  // studnet1.clear();
 }
